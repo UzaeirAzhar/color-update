2029c1f8891bea7649cfbad8aa7bf57c518f5a0c
Uzaeir <paf_uzair@yahoo.com>
2023-05-24 17:54:05 +0200

    chore: update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,31 @@
+<!-- eslint-disable no-unused-vars -->
 <!-- eslint-disable vue/valid-template-root -->
 <template>
-  {{ message }}
+  <div class="header" :style="{backgroundColor: colorArray}">
+    <h1>{{ message }}</h1>
+    <!-- <div class="header" id="emoji">
+      <span>{{ emoji }}</span>
+    </div> -->
+
+    <div class="header" id="subheader">
+      <label class="header" id="label-color">Choose some color</label>
+      <!-- <textarea
+        name="some textarea"
+        id=""
+        cols="30"
+        rows="10"
+        placeholder="input some color"
+        @keyup.enter="handleKeyPress"
+      ></textarea> -->
+      <input
+        type="text"
+        @keyup.enter="handleKeyPress"
+        placeholder="enter some color"
+        v-model="inputText"
+      />
+    </div>
+    {{ colorArray }}
+  </div>
 </template>
 
 <script>
@@ -8,8 +33,38 @@ export default {
   name: "App",
   data() {
     return {
-      message: "Hello Uzair's World 😉",
+      message: "Hello Uzair's World....",
+      emoji: "",
+      inputText: "",
+      colorArray: [],
     };
+  },
+  mounted() {
+    // this.printEmoji();
+  },
+  methods: {
+    // eslint-disable-next-line vue/no-dupe-keys
+    // printEmoji: function () {
+    //   const messageLength = this.message.length;
+    //   const emoji = "😉";
+    //   let counter = 0;
+    //   const interval = setInterval(() => {
+    //     if (counter < messageLength) {
+    //       this.emoji += emoji;
+    //       counter++;
+    //     } else {
+    //       clearInterval(interval);
+    //     }
+    //   }, 500); // Delay between each emoji in milliseconds
+    // },
+    handleKeyPress: function (e) {
+      if (e.key === "Enter" || e.key === " ") {
+        if (this.inputText.trim() !== "") {
+          this.colorArray.push(this.inputText.trim());
+          this.inputText = ""; // Clear the input field
+        }
+      }
+    },
   },
   components: {},
 };
@@ -23,5 +78,33 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.header {
+  font-style: normal;
+  position: relative;
+}
+.subheader {
+  position: fixed;
+  margin-top: 60px;
+}
+.header span {
+  font-style: normal;
+}
+.header label input {
+  position: static;
+}
+#label-color {
+  font-style: normal;
+  font-size: 14px;
+  margin-right: 5px;
+  color: #2c3e50;
+}
+#input-color {
+  font-style: normal;
+  font-size: 12px;
+  color: #2c3e50;
+}
+#emoji {
+  position: relative;
 }
 </style>
