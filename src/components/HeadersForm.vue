cf1cd3ff9aaa6a2f69dc4c0ee48012ddd27964b4
Uzaeir <paf_uzair@yahoo.com>
2023-05-24 18:58:02 +0200

    chore: update file name and content

---
+++ src/components/HeadersForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header" :style="{ color: color }">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: Array,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
