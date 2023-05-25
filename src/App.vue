a359912eee381ddf13696b6e3e3baf5e30b97634
Uzaeir <paf_uzair@yahoo.com>
2023-05-25 09:23:59 +0200

    chore: update App file

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,22 @@
 <!-- eslint-disable no-unused-vars -->
 <!-- eslint-disable vue/valid-template-root -->
 <template>
-  <div class="header" id="subheader">
-    <label class="header" id="label-color">Enter Color</label>
+  <div v-if="!isColorEntered" class="header" id="subheader">
+    <label class="header" id="label-color" data-test-id="enter-color-label">Enter Color</label>
     <input
       type="text"
       @keyup.enter="handleKeyPress"
       placeholder="enter some color"
-      v-model="inputText"
+      v-model="userColor"
+      data-test-id="user-input-field"
     />
+    <p v-if="errorMessage" class="error" data-test-id="error-message">{{ errorMessage }}</p>
   </div>
 
-  <HeadersForm  :message="message" :color="colorArray"/>
+  <div v-else>
+    <HeadersForm :message="message" :color="userColor" />
+    <button @click="goBack" data-test-id="home-button">Home</button>
+  </div>
 </template>
 
 <script>
@@ -23,18 +28,38 @@ export default {
   data() {
     return {
       message: "Hello Uzair's World....",
-      inputText: "",
-      colorArray: [],
+      userColor: "",
+      isColorEntered: false,
+      errorMessage: "",
     };
   },
   methods: {
     handleKeyPress: function (e) {
       if (e.key === "Enter" || e.key === " ") {
-        if (this.inputText.trim() !== "") {
-          this.colorArray.push(this.inputText.trim());
-          this.inputText = ""; // Clear the input field
+        if (this.isValidColor(this.userColor)) {
+          this.isColorEntered = true;
+          this.showError = false;
+        } else {
+          this.showError = true;
+          this.errorMessage =
+            "Invalid color entered. Please enter a valid color.";
         }
       }
+    },
+    isValidColor: function (color) {
+      const colorNames =
+        "aliceblue|antiquewhite|aqua|aquamarine|azure|beige|bisque|black|blanchedalmond|blue|blueviolet|brown|burlywood|cadetblue|chartreuse|chocolate|coral|cornflowerblue|cornsilk|crimson|cyan|darkblue|darkcyan|darkgoldenrod|darkgray|darkgreen|darkkhaki|darkmagenta|darkolivegreen|darkorange|darkorchid|darkred|darksalmon|darkseagreen|darkslateblue|darkslategray|darkturquoise|darkviolet|deeppink|deepskyblue|dimgray|dodgerblue|firebrick|floralwhite|forestgreen|fuchsia|gainsboro|ghostwhite|gold|goldenrod|gray|green|greenyellow|honeydew|hotpink|indianred|indigo|ivory|khaki|lavender|lavenderblush|lawngreen|lemonchiffon|lightblue|lightcoral|lightcyan|lightgoldenrodyellow|lightgray|lightgreen|lightpink|lightsalmon|lightseagreen|lightskyblue|lightslategray|lightsteelblue|lightyellow|lime|limegreen|linen|magenta|maroon|mediumaquamarine|mediumblue|mediumorchid|mediumpurple|mediumseagreen|mediumslateblue|mediumspringgreen|mediumturquoise|mediumvioletred|midnightblue|mintcream|mistyrose|moccasin|navajowhite|navy|oldlace|olive|olivedrab|orange|orangered|orchid|palegoldenrod|palegreen|paleturquoise|palevioletred|papayawhip|peachpuff|peru|pink|plum|powderblue|purple|rebeccapurple|red|rosybrown|royalblue|saddlebrown|salmon|sandybrown|seagreen|seashell|sienna|silver|skyblue|slateblue|slategray|snow|springgreen|steelblue|tan|teal|thistle|tomato|turquoise|violet|wheat|white|whitesmoke|yellow|yellowgreen";
+
+      const colorRegex = new RegExp(
+        `^(#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})|${colorNames})$`,
+        "i"
+      );
+
+      return colorRegex.test(color);
+    },
+    goBack: function () {
+      this.isColorEntered = false;
+      this.userColor = "";
     },
   },
   components: { HeadersForm },
